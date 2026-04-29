6411c5ae651d1209f2ff226c7f439736bf312364
Aksahdj24 <akshadj2005@gmail.com>
2026-04-29 21:31:33 +0530

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Safar Tourism Backend")
 # -------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://safar-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
